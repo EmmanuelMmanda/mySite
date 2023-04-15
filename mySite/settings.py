2d915024db8af02917c4c97f3b5ce744ee53e10f
EmmanuelMmanda <luneya17@gmail.com>
2023-04-15 14:38:54 +0300

    update

--- mySite/settings.py
+++ mySite/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'), 
+        
         # 'OPTIONS': {
         #     'charset': 'utf8mb4',
         #     'use_unicode': True,
