b82e243332d8a67c0c3113ec8d0e2b7f0229b40d
EmmanuelMmanda <luneya17@gmail.com>
2023-04-15 14:58:11 +0300

    reqr

--- mySite/settings.py
+++ mySite/settings.py
@@ -84,18 +84,16 @@ WSGI_APPLICATION = "mySite.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'), 
-        
-        # 'OPTIONS': {
-        #     'charset': 'utf8mb4',
-        #     'use_unicode': True,
-        #     'driver': 'mysql.connector.django',
-        # },
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
     }
 }
 
